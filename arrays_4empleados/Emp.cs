a0dfaefed3a8dbfa213afacb5a9a1556ab739dd7
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:46:32 -0500

    Initial commit

---
+++ arrays_4empleados/Emp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_4empleados
+{
+    internal class Emp
+    {
+        //Propiedades
+        private int[] Sueldos;
+        //Constructor
+        public Emp()
+        {
+            Sueldos = new int[4];
+            for (int i = 0; i < Sueldos.Length; i++)
+            {
+                Console.WriteLine("Ingrese el Sueldo:");
+                string linea = Console.ReadLine();
+                Sueldos[i] = int.Parse(linea);
+            }
+        }
+    }
+}
---
+++ arrays_4empleados/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using arrays_4empleados;
+
+Console.WriteLine("Hello, programa de 4 empleados");
+Emp empleados = new Emp();
+Console.ReadKey(true);
